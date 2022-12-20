910476060856914f96d2be8dd34c1d9b21851c8f
SidiEyel <100689962+SidiEyel@users.noreply.github.com>
2022-12-20 14:25:07 -0800

    Add files via upload

---
+++ LesTp/projet/src/main/java/com/servlet/inscription/Inscription.java
@@ -0,0 +1,43 @@
+package com.servlet.inscription;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Inscription
+ */
+@WebServlet("/inscription")
+public class Inscription extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	public static final String VUE = "/WEB-INF/inscription.jsp";
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Inscription() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		/* Affichage de la page d'inscription */
+		 this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
